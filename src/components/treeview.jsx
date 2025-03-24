0a700f1df3b8fd394ad176450e68c81ffedfe444
CaimeoAI <judiellezinke@yahoo.de>
2025-03-24 21:14:19 +0100

    integrated MenuList component into treeview component and passed menus data as props

--- src/components/treeview.jsx
+++ src/components/treeview.jsx
@@ -5,7 +5,7 @@ export default function Treeview({menus = []}) {
 
     return (
         <div className="treeview-wrapper">
-            
+            <MenuList list={menus}/>
         </div>
     )
 }
