c38985d1563d71e7c58b1ec5fa9c4c92f0905b4d
CaimeoAI <judiellezinke@yahoo.de>
2025-03-25 21:21:00 +0100

    added conditional to menu-item component that recalls it's parent component when referenced item has children and passes item's children data as props

--- src/components/menu-item.jsx
+++ src/components/menu-item.jsx
@@ -1,3 +1,4 @@
+import MenuList from "./menu-list";
 
 
 
@@ -6,6 +7,14 @@ export default function MenuItem({item}) {
     return (
         <li>
             <p>{item.label}</p>
+
+            {
+                item && item.children && item.children.length > 0 ?
+                    <MenuList list={item.children} />
+                    
+                : null
+
+            }
         </li>
     )
 }
